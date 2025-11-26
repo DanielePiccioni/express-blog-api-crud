3058ff5796b76e3fba085e096e74f31067c9fb17
DanielePiccioni <danielepiccioni1@gmail.com>
2025-11-26 13:02:13 +0100

    put the array in the data folder

---
+++ data/posts.js
@@ -0,0 +1,39 @@
+const posts = [
+    {
+        id: 1,
+        title: "Ciambellone",
+        content: "Sarà che una volta le cose erano più semplici...",
+        image: "/imgs/posts/ciambellone.jpeg",
+        tags: ["Dolci", "Torte", "Ricette vegetariane", "Ricette al forno"],
+    },
+    {
+        id: 2,
+        title: "Cracker alla barbabietola",
+        content: "I cracker alla barbabietola sono uno snack stuzzicante...",
+        image: "/imgs/posts/cracker_barbabietola.jpeg",
+        tags: ["Antipasti", "Ricette vegetariane", "Ricette al forno"],
+    },
+    {
+        id: 3,
+        title: "Pasta barbabietola e gorgonzola",
+        content: "La nostra ricetta della pasta barbabietola e gorgonzola...",
+        image: "/imgs/posts/pasta_barbabietola.jpeg",
+        tags: ["Primi piatti", "Ricette vegetariane"],
+    },
+    {
+        id: 4,
+        title: "Pane fritto dolce",
+        content: "Il pane fritto dolce è la versione più antica del french toast...",
+        image: "/imgs/posts/pane_fritto_dolce.jpeg",
+        tags: ["Dolci", "Dolci veloci", "Ricette veloci", "Ricette vegetariane"],
+    },
+    {
+        id: 5,
+        title: "Torta paesana",
+        content: "La torta paesana è un dolce di origine lombarda...",
+        image: "/imgs/posts/torta_paesana.jpeg",
+        tags: ["Dolci", "Dolci al cioccolato", "Torte", "Ricette vegetariane", "Ricette al forno"],
+    },
+];
+
+module.exports = posts;
