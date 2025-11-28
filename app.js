9fdde28a6ac9ac3e76b8bee951161ba36394ac0a
DanielePiccioni <danielepiccioni1@gmail.com>
2025-11-28 12:47:19 +0100

    import notFound middleware in app.js and use it

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const PORT = 3000
 const postsRouter = require('./routers/posts')
+const notFound = require('./middleware/notFound');
 
 app.use(express.static(`public`))
 
@@ -9,6 +10,7 @@ app.use(express.json());
 
 app.use('/posts', postsRouter)
 
+app.use(notFound);
 
 app.listen(PORT, () => {
     console.log(`server running on http://localhost:${PORT}`);
